02ae2480ad2f9d700494ce450f8a428cf0494ea5
40234272 <gtaylor18@qub.ac.uk>
2021-12-10 16:52:32 +0000

    added another proxy backup endpoint, and changed the way proxies are checked

--- src/editor-functions.js
+++ src/editor-functions.js
@@ -16,27 +16,23 @@ async function EditorFunction(func) {
     let proxyList = PROXYLIST;
     let proxyStatus;
 
-    // get the response from the main proxy URL
-    fetch(proxyList['main']).then(function(response) {
-        proxyStatus = response.status;
-        if(proxyStatus == 200){         // If the response is OK, pass the main proxy url to runRequest and continue on with the operation
-            let url = [
-                proxyList['main'],                                              // Proxy URL
-                "/?func=",                                                      // function query
-                func,                                                           // editor function
-                "&text=", encodeURI(document.getElementById('content').value)   // query
-            ].join('');
-            runRequest(url);
-        } else {                        // if a bad response code is returned, use the backup proxy
-            let url = [
-                proxyList['backup'],
-                "/?func=", 
-                func, 
-                "&text=", encodeURI(document.getElementById('content').value)
-            ].join('');
-            runRequest(url);
-        }
-    });
+    // loop through the proxy list and find one that is working
+    for(const[key, value] of Object.entries(proxyList)) {
+        fetch(value).then(function(response) {
+            proxyStatus = response.status;
+            if(proxyStatus == 200){         // If the response is OK, pass the main proxy url to runRequest and continue on with the operation
+                let url = [
+                    value,                                                          // Proxy URL
+                    "/?func=",                                                      // function query
+                    func,                                                           // editor function
+                    "&text=", encodeURI(document.getElementById('content').value)   // query
+                ].join('');
+                runRequest(url);
+            } else {     // if a bad response code is returned, check the next proxy in the list and log a warning to the console
+                console.warn("Proxy " + key + " offline with status code " + proxyStatus + ", trying next in list")           
+            }
+        });
+    }
 
     // separated out the main request to allow for response code checking for proxy
     function runRequest(validURL) {
--- src/proxy-endpoints.js
+++ src/proxy-endpoints.js
@@ -1,5 +1,6 @@
 // External file for proxy urls so they can be easily maintained and re-used
 let PROXYLIST = {
     "main": "http://proxy.40234272.qpc.hal.davecutting.uk",
-    "backup": "http://proxybackup.40234272.qpc.hal.davecutting.uk"
+    "backup": "http://proxybackup.40234272.qpc.hal.davecutting.uk",
+    "backup2": "http://proxybackup2.40234272.qpc.hal.davecutting.uk"
 }
