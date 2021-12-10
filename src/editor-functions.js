f9ca02ffe37f7a4e49c698b012217984ba03f3ae
40234272 <gtaylor18@qub.ac.uk>
2021-12-10 15:56:21 +0000

    added frontend service failure handling

--- src/editor-functions.js
+++ src/editor-functions.js
@@ -11,32 +11,46 @@ function UpdateAnswer(response) {
     document.getElementById('output').value = answer;
 }
 
-// one main function that takes a function type as an argument, and gets the URL from an external list
-function EditorFunction(func) {
+// function that takes a editor function type as an argument, and gets the proxy URL from an external list based on response status
+async function EditorFunction(func) {
+    let proxyList = PROXYLIST;
+    let proxyStatus;
 
-    let proxy = "http://proxy.40234272.qpc.hal.davecutting.uk"
-    let xhttp = new XMLHttpRequest();
+    // get the response from the main proxy URL
+    fetch(proxyList['main']).then(function(response) {
+        proxyStatus = response.status;
+        if(proxyStatus == 200){         // If the response is OK, pass the main proxy url to runRequest and continue on with the operation
+            let url = [
+                proxyList['main'],                                              // Proxy URL
+                "/?func=",                                                      // function query
+                func,                                                           // editor function
+                "&text=", encodeURI(document.getElementById('content').value)   // query
+            ].join('');
+            runRequest(url);
+        } else {                        // if a bad response code is returned, use the backup proxy
+            let url = [
+                proxyList['backup'],
+                "/?func=", 
+                func, 
+                "&text=", encodeURI(document.getElementById('content').value)
+            ].join('');
+            runRequest(url);
+        }
+    });
 
-    if (func == undefined){
-        document.getElementById('output').value = "Function Undefined";
-    } else {
-        xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            UpdateAnswer(this.response);
-        }};
+    // separated out the main request to allow for response code checking for proxy
+    function runRequest(validURL) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.open("GET", validURL);
+        if (func == undefined){
+            document.getElementById('output').value = "Function Undefined";
+        } else {
+            xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                UpdateAnswer(this.response);
+            }};
+        }
+        xhttp.send();
     }
-
-    let url = [
-        // Proxy URL
-        proxy,
-        //function query
-        "/?func=", 
-        // editor function
-        func, 
-        // query
-        "&text=", encodeURI(document.getElementById('content').value)
-    ].join('');
-
-    xhttp.open("GET", url);
-    xhttp.send();
+    
 }
---
+++ src/proxy-endpoints.js
@@ -0,0 +1,5 @@
+// External file for proxy urls so they can be easily maintained and re-used
+let PROXYLIST = {
+    "main": "http://proxy.40234272.qpc.hal.davecutting.uk",
+    "backup": "http://proxybackup.40234272.qpc.hal.davecutting.uk"
+}
